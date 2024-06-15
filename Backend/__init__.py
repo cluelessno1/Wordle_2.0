e8d02874766a210cdaa6c8207a3c3e99ff18ea8c
Shashwat Agrawal <shashwat1999@gmail.com>
2024-06-15 17:32:28 +0530

    Added logging level to be tajken from the .env file

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,5 +1,21 @@
 from . import *
 import logging
+import os
+from dotenv import dotenv_values
 
-# Configure the logging
-logging.basicConfig(filename='wordle_2_backend.log', filemode='a', level=logging.INFO, format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the relative path to the .env file
+env_path = os.path.join(current_dir, '.env')
+
+# Load the data from the .env file
+config = dotenv_values(env_path)
+
+# Set up logging with the level from the .env file
+logging_level = getattr(logging, config['LOGGING_LEVEL'].upper(), None)
+if logging_level is not None:
+    # Configure the logging
+    logging.basicConfig(filename='wordle_2_backend.log', filemode='a', level=logging.INFO, format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+else:
+    logging.warning('Invalid LOGGING_LEVEL value in .env file')
